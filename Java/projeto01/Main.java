16c53c1bb91c6fb9ca9faadd87c6e22850df383b
EnzoHashinokutiXavier <enzokuti.xavier@gmail.com>
2026-09-09 18:44:47 -0400

    Java Study

---
+++ Java/projeto01/Main.java
@@ -0,0 +1,10 @@
+import model.ContaCorrente;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ola mundo !");
+
+        ContaCorrente contaCorrente = new ContaCorrente();
+        contaCorrente.saldo();
+    }
+}
---
+++ Java/projeto01/model/ContaCorrente.java
@@ -0,0 +1,7 @@
+package model;
+
+public class ContaCorrente {
+    public void saldo(){
+        System.out.println("Saldo: 2000");
+    }
+}
