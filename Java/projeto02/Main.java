9a73a2e4c411d17217eb42dc31ac4153d6567734
EnzoHashinokutiXavier <enzokuti.xavier@gmail.com>
2026-09-12 18:20:47 -0400

    Java Variables Study

---
+++ Java/projeto02/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+
+        // Tipo de dado + identificação + valor atribuido
+        String nome = "Enzo Xavier";
+
+        System.out.println(nome);
+        
+    }
+}
