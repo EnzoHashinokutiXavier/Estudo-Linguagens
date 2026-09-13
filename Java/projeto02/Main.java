50bb6658109a8bbeabc672acf21d7db8bcb854de
EnzoHashinokutiXavier <enzokuti.xavier@gmail.com>
2026-09-13 15:36:21 -0400

    Java Input Study

--- Java/projeto02/Main.java
+++ Java/projeto02/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
 
@@ -13,5 +15,24 @@ public class Main{
         final String NOME = "Joao";
         System.out.println(NOME);
 
+        // Contatenaçao
+        System.out.println("Meu nome é " + nome + " " + sobrenome);
+
+        // Entrada do usuário
+        try (Scanner entradaUsuario = new Scanner(System.in)){
+            System.out.println("Qual é o seu nome ? ");
+            nome = entradaUsuario.nextLine();
+            System.out.println("Ola " + entradaUsuario);
+            System.out.println("Qual é a sua idade ? ");
+            Byte idade = entradaUsuario.nextByte();
+            if (idade >= 18){
+                System.out.println("Você é maior de idade !");
+            }
+            else {
+                System.out.println( "Você é menor de idade");
+            }
+            String idadeString = idade.toString();
+            System.out.println("Sua idade é " + idadeString);
+        }
     }
 }
