dc9b29ec801abdc8c566853072abdc4beb00335f
EnzoHashinokutiXavier <enzokuti.xavier@gmail.com>
2026-09-12 20:08:38 -0400

    Java Var Study

--- Java/projeto02/Main.java
+++ Java/projeto02/Main.java
@@ -2,9 +2,13 @@ public class Main{
     public static void main(String[] args) {
 
         // Tipo de dado + identificação + valor atribuido
-        String nome = "Enzo Xavier";
+        String nome = "Enzo";
 
         System.out.println(nome);
+
+        // Declarar variavel sem definir tipo previamente
+        var sobrenome = "Xavier";
+        System.out.println(sobrenome);
         
     }
 }
