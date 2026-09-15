57253d9751d59f2651e41ec2f26743a671280ea5
EnzoHashinokutiXavier <enzokuti.xavier@gmail.com>
2026-09-15 16:18:22 -0400

    Java Object Study

---
+++ Java/projeto04/Main.java
@@ -0,0 +1,14 @@
+import poo.Carro;
+//import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Carro carro = new Carro(); 
+        carro.acelerar();
+        carro.frear();
+        carro.abastecer();
+
+        //Scanner entradaUsuario = new Scanner(System.in);
+    }
+}
---
+++ Java/projeto04/poo/Carro.java
@@ -0,0 +1,20 @@
+package poo;
+
+public class Carro {
+    // Propriedades | atributos | campos | variaveis de instancia 
+    String cor;
+    String placa;
+
+    // Método - comportamento, açoes
+    public void acelerar(){
+        System.out.println("Carro em movimento");
+    }
+
+    public void frear(){
+        System.out.println("Carro parou");
+    }
+
+    public void abastecer(){
+        System.out.println("Carro abastecendo");
+    }
+}
